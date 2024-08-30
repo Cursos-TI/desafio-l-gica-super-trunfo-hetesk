86e6698d4e064230c63efcdbb1f56b7489234546
hetesk <105890759+hetesk@users.noreply.github.com>
2024-08-29 21:41:38 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -9,7 +9,23 @@ int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
-    
+    int Acodcidade = 0;
+    char Anome[20] = "";
+    int Apopulacao = 0; // apesar deste curso não ensinar DEVES-SE INICIALIZAR A VARIAVEL COM UM VALOR PARA NÃO CONTER LIXO!
+    int Aarea  = 0;
+    int Apib = 0;
+    int Anpontoturistico=0;
+
+    int Bcodcidade = 0;
+    char Bnome[20] = "";
+    int Bpopulacao = 0; // apesar deste curso não ensinar DEVES-SE INICIALIZAR A VARIAVEL COM UM VALOR PARA NÃO CONTER LIXO!
+    int Barea  = 0;
+    int Bpib = 0;
+    int Bnpontoturistico=0;
+
+
+
+
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
@@ -18,7 +34,53 @@ int main() {
     // Exemplo:
     // printf("Digite o código da cidade: ");
     // scanf("%s", codigo);
-    // 
+    printf("\nINSIRA OS DADOS DA CARTA A");
+    printf("\nCodigo da cidade > ");
+    scanf("%d", &Acodcidade); // esse curso poderia ensinar que o & serve para fornescer o ponteiro de armazenamento da variavel
+
+    printf("\nNome > ");
+    scanf("%s", Anome);  
+
+    printf("\nPopulação > ");
+    scanf("%d", &Apopulacao);
+
+    printf("\nArea > ");
+    scanf("%d", &Aarea);
+
+    printf("\nPIB > ");
+    scanf("%d", &Apib);
+
+    printf("\nNumero de pontos turísticos > ");
+    scanf("%d", &Anpontoturistico);
+
+
+    printf("\n\n\n\  ********     n\n\n\nINSIRA OS DADOS DA CARTA B");
+    printf("\nCodigo da cidade > ");
+    scanf("%d", &Bcodcidade); // esse curso poderia ensinar que o & serve para fornescer o ponteiro de armazenamento da variavel
+
+    printf("\nNome > ");
+    scanf("%s", Bnome);  
+
+    printf("\nPopulação > ");
+    scanf("%d", &Bpopulacao);
+
+    printf("\nArea > ");
+    scanf("%d", &Barea);
+
+    printf("\nPIB > ");
+    scanf("%d", &Bpib);
+
+    printf("\nNumero de pontos turísticos > ");
+    scanf("%d", &Bnpontoturistico);
+
+
+if (Apib > Bpib) 
+	{printf("CARTA A VENCEDORA POIS TEM MAIOR PIB");}
+    else 
+	{printf("CARTA A VENCEDORA POIS TEM MAIOR PIB");};
+	
+
+
     // (Repita para cada propriedade)
 
     // Comparação de Cartas:
@@ -40,4 +102,3 @@ int main() {
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
     return 0;
-}
